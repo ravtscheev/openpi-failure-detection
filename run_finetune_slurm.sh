3302ff580c650f52d1d8e4dd5b6760ad61d48d60
ravtscheev <git@ravtscheev.com>
2026-02-13 22:09:14 +0100

    Update SLURM script for pi0 finetuning with new resource allocations and image paths

--- run_finetune_slurm.sh
+++ run_finetune_slurm.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 #SBATCH --job-name=pi0_finetune
-#SBATCH --partition=short1d          # Guaranteed 24h runtime
-#SBATCH --gpus-per-node=a100_80gb:1  # Required for >22GB VRAM
-#SBATCH --mem=32G
-#SBATCH --cpus-per-task=4
-#SBATCH --output=/mnt/vanishing_data/%u/logs/pi0_%j.log
+#SBATCH --time=02-00:00:00
+#SBATCH --partition=short7d
+#SBATCH --output=/mnt/home/is357/logs/openpi\_train\_%j.out
+#SBATCH --error=/mnt/home/is357/logs/openpi\_train\_%j.err
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:1
+
 
 # Paths (Update these!)
 TRAIN_CONFIG=pi0_ur5_merged_eef_50
 EXP_NAME="$TRAIN_CONFIG"
 DATASET_NAME="ravtscheev/merged-tasks-eef-eef"
 
-IMAGE_NAME="openpi\_train:latest"
+IMAGE_NAME="openpi_finetune:latest"
 WAND_KEY=""
 
 PROJECT_ROOT=/mnt/home/$USER/checkout/openpi
-DATA_DIR="/mnt/vanishing_data/$USER/datasets/merged-tasks-eef-eef"
-BASE_MODEL_DIR="/mnt/vanishing_data/$USER/base_model"
+BASE_MODEL_DIR="/mnt/home/$USER/base_model"
 
 
 echo "1. Loading Docker Image from $IMAGE_PATH..."
 # Must load image every time in rootless SLURM
-docker image load --input ~/openpi\_train.tar.gz
+docker image load --input ~/openpi_finetune.tar.gz
 
 echo "2. Starting Container..."
 # -v mounts host folders to container folders
@@ -32,10 +36,10 @@ docker run --rm \
     --gpus all \
     --network host \
     -v $PROJECT_ROOT:/app
-    -v "$DATA_DIR":/root/.cache/huggingface/lerobot/$DATASET_NAME
     -v "$BASE_MODEL_DIR":/root/.cache/openpi
     -e TRAIN_CONFIG=$TRAIN_CONFIG
     -e EXP_NAME=$EXP_NAME
+    -e SKIP_STATS=true
     -e TRAIN_ARGS=--overwrite
     -e WANDB_API_KEY=$WAND_KEY
     $IMAGE\_NAME
